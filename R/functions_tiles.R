36a94cb2a1f265e75effe3284c7345c94888a66a
XiaquZ <xiaqu.zhou@gmail.com>
2026-03-12 15:07:34 +0100

    Create targets workflow with crew.cluster.

---
+++ R/functions_tiles.R
@@ -0,0 +1,85 @@
+match_cur_fut_files <- function(cur_files, fut_files) {
+  to_species_key <- function(x) {
+    x <- basename(x)
+    x <- sub("\\.tif$", "", x, ignore.case = TRUE)
+    x <- sub("^binary[_ ]+", "", x, ignore.case = TRUE)
+    x <- sub("(_CurrentActual|_CurrentAtual|_Current)$", "", x, ignore.case = TRUE)
+    x <- sub("(_FuturePotentialReachable|_FuturePotential|_FutureReachable|_Future)$", "", x, ignore.case = TRUE)
+    x <- gsub("[[:space:]]+", "_", x)
+    x <- gsub("_+", "_", x)
+    x <- gsub("^_|_$", "", x)
+    x
+  }
+  
+  cur_key <- to_species_key(cur_files)
+  fut_key <- to_species_key(fut_files)
+  
+  if (!setequal(cur_key, fut_key)) {
+    stop(
+      "Species sets differ.\n",
+      "Missing in future: ", paste(setdiff(cur_key, fut_key), collapse = ", "), "\n",
+      "Missing in current: ", paste(setdiff(fut_key, cur_key), collapse = ", ")
+    )
+  }
+  
+  list(
+    cur = cur_files,
+    fut = fut_files[match(cur_key, fut_key)]
+  )
+}
+
+make_tile_grid <- function(eu_shape, cellsize) {
+  grid <- sf::st_make_grid(
+    eu_shape, 
+    cellsize = c(cellsize, cellsize)
+    ) |>
+    sf::st_as_sf()
+}
+
+is_jointly_empty <- function(r1, r2) {
+  mx1 <- suppressWarnings(terra::global(r1, "max", na.rm = TRUE)[1, ])
+  mx2 <- suppressWarnings(terra::global(r2, "max", na.rm = TRUE)[1, ])
+  
+  empty1 <- all(is.na(mx1) | mx1 <= 0)
+  empty2 <- all(is.na(mx2) | mx2 <= 0)
+  
+  empty1 && empty2
+}
+
+process_one_tile <- function(i, grid_sf, cur_files, fut_files, out_dir_cur, out_dir_fut) {
+  
+  message("------ Tile ", i, " ------")
+  
+  dir.create(out_dir_cur, recursive = TRUE, showWarnings = FALSE)
+  dir.create(out_dir_fut, recursive = TRUE, showWarnings = FALSE)
+  
+  cur_stack <- terra::rast(cur_files)
+  fut_stack <- terra::rast(fut_files)
+  
+  tile_vect <- terra::vect(grid_sf[i, ])
+  e <- terra::ext(tile_vect)
+  
+  message("Cropping tile ", i)
+  cur_tile <- terra::crop(cur_stack, e, snap = "out")
+  fut_tile <- terra::crop(fut_stack, e, snap = "out")
+  
+  if (is_jointly_empty(cur_tile, fut_tile)) {
+    return(character(0))
+  }
+  
+  fcur <- file.path(out_dir_cur, sprintf("tile_current_%03d.tif", i))
+  ffut <- file.path(out_dir_fut, sprintf("tile_future_%03d.tif", i))
+  
+  wopt <- list(
+    datatype = "INT1U",
+    gdal = c("COMPRESS=LZW", "TILED=YES", "BIGTIFF=YES")
+  )
+  
+  terra::writeRaster(cur_tile, fcur, overwrite = TRUE, wopt = wopt)
+  terra::writeRaster(fut_tile, ffut, overwrite = TRUE, wopt = wopt)
+  rm(cur_tile, fut_tile)
+  gc()
+  message("Finished tile ", i)
+  
+  c(fcur, ffut)
+}
---
+++ _targets.R
@@ -0,0 +1,105 @@
+# Created by use_targets().
+# Follow the comments below to fill in this target script.
+# Then follow the manual to check and run the pipeline:
+#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
+
+# Load packages required to define the pipeline:
+library(targets)
+library(crew)
+library(crew.cluster)
+
+## Running on HPC with Slurm:
+project_dir <- "/vsc-hard-mounts/leuven-data/348/vsc34871/MakeTiles_CutFutBinaryStacks"
+
+dir.create(file.path(project_dir, "crew_scripts"), recursive = TRUE, showWarnings = FALSE)
+dir.create(file.path(project_dir, "logs"), recursive = TRUE, showWarnings = FALSE)
+
+tar_option_set(
+  packages = c("terra", "sf"),
+  memory = "transient",
+  garbage_collection = 1,
+  controller = crew_controller_slurm(
+    name = "MakeTiles",
+    workers = 2,
+    seconds_idle = 60,
+    seconds_wall = 10 * 3600,
+    options_cluster = crew_options_slurm(
+      script_directory = file.path(project_dir, "crew_scripts"),
+      cpus_per_task = 72,
+      n_tasks = 1,
+      memory_gigabytes_per_cpu = 3,
+      time_minutes = 10 * 60,
+      partition = "batch",
+      log_output = file.path(project_dir, "logs", "crew_%A.out"),
+      log_error  = file.path(project_dir, "logs", "crew_%A.err"),
+      script_lines = c(
+        "#SBATCH -A lp_climateplants",
+        "#SBATCH -M wice",
+        "source $VSC_HOME/.bashrc",
+        "source activate VoCC_R_new",
+        "cd $VSC_DATA/MakeTiles_CutFutBinaryStacks",
+        "export OMP_NUM_THREADS=1",
+        "export GDAL_NUM_THREADS=1"
+      )
+    )
+  )
+)
+
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source()
+# tar_source("other_functions.R") # Source other scripts as needed.
+
+# Replace the target list below with your own:
+list(
+  tar_target(
+    cur_files,
+    list.files(
+      "I:/DATA/SDM_current/results/Binary_SpeciesCurrentAtual/",
+      pattern = "\\.tif$",
+      full.names = TRUE
+    )
+  ),
+  tar_target(
+    fut_files,
+    list.files(
+      "I:/DATA/SDM_future/Binary_FutureReachableDis/",
+      pattern = "\\.tif$",
+      full.names = TRUE
+    )
+  ),
+  tar_target(
+    matched_files,
+    match_cur_fut_files(cur_files, fut_files)
+  ),
+  tar_target(
+    eu_shape,
+    {
+      x <- sf::read_sf("I:/EUshap/Europe.shp")
+      sf::st_buffer(x, 1)
+    }
+  ),
+  tar_target(
+    grid_sf,
+    make_tile_grid(
+      eu_shape = eu_shape,
+      cellsize = 6e5
+    )
+  ),
+  tar_target(
+    tile_index,
+    seq_len(nrow(grid_sf))
+  ),
+  tar_target(
+    tile_pair_files,
+    process_one_tile(
+      i = tile_index,
+      grid_sf = grid_sf,
+      cur_files = matched_files$cur,
+      fut_files = matched_files$fut,
+      out_dir_cur = "I:/DATA/SDM_current/results/Binary_CurrentActual_tiles/",
+      out_dir_fut = "I:/DATA/SDM_future/Binary_FutureReachable_tiles/"
+    ),
+    pattern = map(tile_index),
+    format = "file"
+  )
+)
